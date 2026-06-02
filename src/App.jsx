1713fc0fe7cb8e570a4e4608d9d1a9d4924dd97b
kghidella-real <kierenghidella@gmail.com>
2026-06-02 13:17:59 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,12 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import HomePage from "./pages/HomePage"
 import GolfPage from "./pages/GolfPage"
-import WedgesPage from "./pages/WedgesPage"
-import IronsPage from "./pages/IronsPage"
-import DriversPage from "./pages/DriversPage"
 import AboutPage from "./pages/AboutPage"
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-gray-50 flex flex-col">
+      <div className="min-h-screen flex flex-col" style={{ background: '#F7F6F3' }}>
         <Header />
         <main className="flex-1">
           <Routes>
